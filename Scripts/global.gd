c3bdd9bedcbaede45398f2a3954d8bcd03506df9
Jean-Nicolas Thurre <jnthurre@outlook.com>
2026-03-21 12:05:28 +0100

    add global keys_state

---
+++ Scripts/global.gd
@@ -0,0 +1,38 @@
+extends Node
+
+var keys_state = {
+	# Player 1
+	"q" : {
+		"player" : 1,
+		"line" : 0,
+		"col" : 0,
+		"state" : true,
+		"multiplicator" : 1,
+		"pressed" : false
+	},
+	"w" : {
+		"player" : 1,
+		"line" : 0,
+		"col" : 1,
+		"state" : false,
+		"multiplicator" : 1,
+		"pressed" : false
+	},
+	# Player 2
+	"z" : {
+		"player" : 2,
+		"line" : 0,
+		"col" : 0,
+		"state" : true,
+		"multiplicator" : 1,
+		"pressed" : false
+	},
+	"u" : {
+		"player" : 2,
+		"line" : 0,
+		"col" : 1,
+		"state" : false,
+		"multiplicator" : 1,
+		"pressed" : false
+	},
+}

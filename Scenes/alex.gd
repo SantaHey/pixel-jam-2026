50ec8b1b85c5d6aa64adbd2cfaf5365a17e11926
Alexandre <alexandre.venturi@outlook.com>
2026-03-21 15:24:14 +0100

    feat : score increment with key

--- Scenes/alex.gd
+++ Scenes/alex.gd
@@ -8,4 +8,24 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	
+	if Input.is_action_just_pressed("Q") :
+		if Global.keys_state["q"]["state"]==true  :
+			Global.score_j1 += 1*Global.keys_state["q"]["multiplicator"]
+		Tools.showdebugtext("Q",Global.score_j1)
+	
+	if Input.is_action_just_pressed("W") :
+		if Global.keys_state["w"]["state"]==true  :
+			Global.score_j1 += 1*Global.keys_state["w"]["multiplicator"]
+		Tools.showdebugtext("W", Global.score_j1)
+		
+	if Input.is_action_just_pressed("Z") :
+		if Global.keys_state["z"]["state"]==true  :
+			Global.score_j2 += 1*Global.keys_state["z"]["multiplicator"]
+		Tools.showdebugtext("Z", Global.score_j2)
+		
+	if Input.is_action_just_pressed("U") :
+		if Global.keys_state["u"]["state"]==true  :
+			Global.score_j2 += 1*Global.keys_state["u"]["multiplicator"]
+		Tools.showdebugtext("U", Global.score_j2)             
+		
